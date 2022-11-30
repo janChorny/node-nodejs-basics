c8bf6e32bb0d05cc5f896dd952d1866f2b491fe0
janChorny <janchorny@tut.by>
2022-12-01 00:15:59 +0300

    feat: add create task solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import {readFile} from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+    try {
+      const text = await readFile("./src/fs/files/fresh.txt", {encoding: "utf8"});
+      console.log(text);
+    } catch (err) {
+      console.log(err.message)
+    }
 };
 
 await create();
