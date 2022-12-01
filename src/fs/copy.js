af963e45feb69c8c3363a6cb790699577a124e1a
janChorny <janchorny@tut.by>
2022-12-01 22:26:30 +0300

    feat: add task 2 solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const copyFrom = ('src/fs/files');
+  const copyTo = ('src/fs/files_copy');
+
+  try {
+    const files = await fs.readdir(copyFrom);
+    await fs.mkdir(copyTo)
+
+    for (let file of files) {
+      await fs.copyFile(path.join(copyFrom, file), path.join(copyTo, file))
+    };
+
+  } catch (error) {
+    console.log('FS operation failed');
+  }
 };
 
 copy();
