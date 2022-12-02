795d923a838b092fe9a848ec4232ff4b2fe31213
janChorny <janchorny@tut.by>
2022-12-03 00:50:19 +0300

    feat: add hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const path = 'src/hash/files/fileToCalculateHashFor.txt';
+  try {
+    const file = await readFile(path);
+    const res = createHash('sha256').update(file).digest('hex');
+    console.log(res);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await calculateHash();
