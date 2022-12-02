374f5a4f00f8b1ee10a33c15a3f9c11b58353794
janChorny <janchorny@tut.by>
2022-12-02 15:19:26 +0300

    feat: add read task solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    try {
+      const path = 'src/fs/files/fileToRead.txt';
+      const fileData = await fs.readFile(path, 'utf8');
+      console.log(fileData);
+    } catch (error) {
+      console.log('FS operation failed');
+    }
 };
 
 await read();
