1a9fb9a309e7343f9734e9737111261ba4518add
janChorny <janchorny@tut.by>
2022-12-03 14:33:32 +0300

    feat: add transform task solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+import { stdin, stdout } from "process";
+
+const myTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.toString().split("").reverse().join(""))
+  }
+});
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    stdin.pipe(myTransform).pipe(stdout);
+  } catch (error) {
+    console.log(error.message)
+  }
 };
 
 await transform();
