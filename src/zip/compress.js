24d7c30f748716637728c445761328436c807426
janChorny <janchorny@tut.by>
2022-12-03 20:29:56 +0300

    feat: add compress and decompress tasks solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import {createGzip} from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const gzip = createGzip();
+    const inp = createReadStream('src/zip/files/fileToCompress.txt');
+    const out = createWriteStream('src/zip/files/fileToCompress.txt.gz');
+    inp.pipe(gzip).pipe(out);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const unzip = createUnzip();
+    const inp = createReadStream('src/zip/files/fileToCompress.txt.gz');
+    const out = createWriteStream('src/zip/files/fileToCompress1.txt');
+    inp.pipe(unzip).pipe(out);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await decompress();
