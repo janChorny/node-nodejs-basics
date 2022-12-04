a712667f8fc93edf2d92bea7feb67342674aa3d3
janChorny <janchorny@tut.by>
2022-12-04 20:26:43 +0300

    feat: clear mistakes from fs tasks

--- src/fs/list.js
+++ src/fs/list.js
@@ -2,7 +2,7 @@ import * as fs from 'fs/promises';
 
 const list = async () => {
     try {
-      const path = 'src/fs/files/fileses';
+      const path = 'src/fs/files';
       const files = await fs.readdir(path);
       console.log(files);
     } catch (error) {
--- src/fs/read.js
+++ src/fs/read.js
@@ -2,7 +2,7 @@ import * as fs from 'fs/promises';
 
 const read = async () => {
     try {
-      const path = 'src/fs/files/fileToRead.twt';
+      const path = 'src/fs/files/fileToRead.txt';
       const fileData = await fs.readFile(path, 'utf8');
       console.log(fileData);
     } catch (error) {
