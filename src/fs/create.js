b52d48c2be59e54682d61fe512e0929cc99c477b
janChorny <janchorny@tut.by>
2022-12-13 16:45:17 +0300

    feat: modify fs create task after crosscheck

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,18 +1,40 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
+import { access } from 'fs/promises';
 
 const file = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(file);
 const filePath = path.join(__dirname, 'files', 'fresh.txt');
 const message = `I am fresh and young`;
+const color = {
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+};
+const isPathExist = async (path) => {
+  try {
+    await access(path);
+
+    return true;
+  } catch {
+    return false;
+  }
+};
 
 const create = async () => {  
   try {
-    await fs.writeFile(filePath, message, {flag: "wx"});
-  } catch (error) {
-    throw new Error('FS operation failed');
+    const isPath = await isPathExist(filePath);
+
+    if (isPath) {
+      throw new Error('FS operation failed');
+    }
+
+    await fs.writeFile(filePath, message, { flag: "wx" });
+    console.log(`${color.green}File created`);
+  } catch (err) {
+    console.log(`${color.red}${err}`);
   }
+
 };
 
 await create();
